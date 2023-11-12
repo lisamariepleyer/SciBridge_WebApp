8fc8df1cccbd2adb75b669550790016445e16d20
lisamariepleyer <Lisa-Marie Pleyer>
2023-11-12 02:37:43 +0100

    add Start button which redirects user to question 1 in /plain or /feedback view; close #3; close #6

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 import './HomePage.scss';
 import '../styles/buttons.scss'
 
 function HomePage() {
+    let [pageStyling, setPageStyling] = useState('');
+
+    let navigation = useNavigate();
+
+    const redirectToRandomPageStyling = () => {
+        const randomInt = Math.floor(Math.random() * 2);
+
+        if (randomInt === 0) {
+            navigation('/plain');
+        } else {
+            navigation('/feedback');
+        }
+
+        console.log(randomInt);
+        console.log(pageStyling);
+    };
+
     return (
         <div className="HomePage">
             <div className="button-container">
                 <Link to="/plain" className="comic-button">Schlicht</Link>
                 <Link to="/feedback" className="comic-button">Feedback</Link>
+            </div>
+            <div className="button-container">
+                <button
+                    className="comic-button"
+                    onClick={() => redirectToRandomPageStyling()}
+                >
+                    Start
+                </button>
             </div>
         </div>
     );

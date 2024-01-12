70750ae349a3322539d2358df556f24d6e51eec1
lisamariepleyer <lisaminusmarie.p@gmail.com>
2024-01-12 23:39:58 +0100

    add test for routing and navigation

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders home page', () => {
+  render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+  );
+  expect(screen.getByText(/Hallo und herzlich Willkommen bei SciBridge!/i)).toBeInTheDocument();
 });
